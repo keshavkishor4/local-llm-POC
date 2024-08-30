3b8e4121c49fc1f24ac927d0964a4df7cf438724
keshavkishor4 <34016462+keshavkishor4@users.noreply.github.com>
2024-08-30 09:43:53 +0000

    This is my llm learning

---
+++ chat.py
@@ -0,0 +1,43 @@
+from typing import List
+from ctransformers import AutoModelForCausalLM
+
+# orca Model
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+# # LLAMA Model
+# llm = AutoModelForCausalLM.from_pretrained(
+#     "TheBloke/Llama-2-7b-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf"
+# )
+# prompt = "Hey, How are you?"
+
+# print(llm(prompt))
+
+# prompt = "What is the name of the capital city of India. Please respond with the city name only and then stop talking. LLM answered: "
+# print(prompt + llm(prompt))
+
+def get_prompt(instruction: str, history: List[str] = None) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if history is not None:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question:-"
+    prompt += f"{instruction}\n\n### Response:\n"
+    # prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
+    print(f"Prompt created: {prompt}")
+    return prompt
+
+history = []
+question = "Which city is the capital of India?"
+prompt = get_prompt(question)
+answer = ""
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+    answer += word
+history.append(answer)
+print()
+
+question = "And which is of the united states?"
+prompt = get_prompt(question, history)
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+print()
