6c0668c01caa85d08deed8a06076406a9c0875b8
keshavkishor4 <34016462+keshavkishor4@users.noreply.github.com>
2024-09-03 11:59:18 +0000

    Adding chat GPT like functionality

--- chat.py
+++ chat.py
@@ -2,10 +2,7 @@ from typing import List
 import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
-# orca Model
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
+
 # # LLAMA Model
 # llm = AutoModelForCausalLM.from_pretrained(
 #     "TheBloke/Llama-2-7b-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf"
@@ -20,7 +17,7 @@ llm = AutoModelForCausalLM.from_pretrained(
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question:-"
     prompt += f"{instruction}\n\n### Response:\n"
     # prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
@@ -29,8 +26,28 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
-    await cl.Message(response).send()
+    # response = f"Hello, you just sent: {message.content}!"
+    message_history = cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
+    # response = llm(prompt)
+    # await cl.Message(response).send()
+
+@cl.on_chat_start
+def on_chat_start():
+    # orca Model
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
 """
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
@@ -53,9 +70,10 @@ for word in llm(prompt, stream=True):
 history.append(answer)
 print()
 
-question = "And which is of the united states?"
+question = "And what  is the capital of united states?"
 prompt = get_prompt(question, history)
 for word in llm(prompt, stream=True):
     print(word, end="", flush=True)
 print()
+
 """
